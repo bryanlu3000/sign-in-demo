3255f576c6f4e61545afdd1121257a3ac633f65f
Bryan <bryan2017canada@gmail.com>
2022-05-26 12:47:11 -0400

    modified server.js on logout response

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -73,7 +73,7 @@ export default function SignUp() {
           if (res.status === 201) {
             setSuccess(true);
           }
-          console.log(res.data);
+          console.log(res.data.message);
         })
         .catch(err => {
           if (!err?.response) {
